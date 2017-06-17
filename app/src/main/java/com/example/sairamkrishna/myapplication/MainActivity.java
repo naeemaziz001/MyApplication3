6e7bdfae0523c8cee2555015e53c3c8cc5d7e199
Naeem Aziz <naeemaziz@Naeems-MacBook-Pro.local>
2017-06-17 13:03:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sairamkrishna/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.sairamkrishna.myapplication;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Play Media",
+                "FlashLight",
+                "Find My Phone",
+                "Send Location",
+                "Notify When Charged",
+                "Catch Thief",
+                "Notify when Power connects",
+                "working..."
+        };
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition     = position;
+
+
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+            }
+
+
+
+        });
+    }
+
+    }
+
+
+
